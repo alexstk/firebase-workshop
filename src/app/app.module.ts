72e2e177a53c6f99d8e754e7f316199620435eba
DESKTOP-OQV2L16\Alex <alexstk@hotmail.com>
2018-09-06 17:05:22 -0500

    initial commit or whatever else you want

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './message/message.component';
+import { InputComponent } from './input/input.component';
+
+var config = {
+  apiKey: "AIzaSyCfHrCsFlqqSKv8rF-9ggA7VrjXLrE4M5o",
+  authDomain: "chat-firebase-city.firebaseapp.com",
+  databaseURL: "https://chat-firebase-city.firebaseio.com",
+  projectId: "chat-firebase-city",
+  storageBucket: "chat-firebase-city.appspot.com",
+  messagingSenderId: "36220629870"
+};
+//
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    MessageComponent,
+    InputComponent
   ],
   imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     BrowserModule
   ],
   providers: [],
